faa23e84a4fd1fce0f6a1a46c84f5866e1bef686
yakshaG <audit536@gmail.com>
2022-07-24 11:31:21 +0000

    Deploying to gh-pages from @ yakshaG/project-pratima@26d91dd4b3d167c110fe3274a169fc3d75a85400 🚀

---
+++ _next/static/chunks/pages/icons-7c392ca32f8e01fa.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[912],{8866:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/icons",function(){return c(6852)}])}},function(a){a.O(0,[2,32,755,774,888,179],function(){var b;return a(a.s=8866)}),_N_E=a.O()}])
