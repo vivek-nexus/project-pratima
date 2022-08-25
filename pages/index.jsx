fc2e3bfca5919418540a264c014806453f8bf9e8
Vivek <audit536@gmail.com>
2022-08-25 09:36:09 +0530

    fix: :bug: Update title and project name

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,9 +17,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Project pratima</title>
+        <title>Project Pratima</title>
         <meta name="description" content="Payment icons for India" />
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
