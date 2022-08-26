ea5108e5db3fd92fefe054f0e8e40147c75a4671
Vivek <audit536@gmail.com>
2022-08-26 10:50:05 +0530

    feat: :sparkles: Add env based variable management

--- constants/imageUrlPrefix.js
+++ constants/imageUrlPrefix.js
@@ -1,7 +1,3 @@
-// // To bring back this file into git tree, run git update-index --no-skip-worktree imageUrlPrefix.js
-
-// Sample URL prefix: https://yakshaG.github.io/project-pratima
-// Do not add the slash at the last
-const imageimageUrlPrefix = "https://yakshaG.github.io/project-pratima";
+const imageimageUrlPrefix = process.env.IMAGE_PREFIX;
 
 export default imageimageUrlPrefix;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    // Sample URL prefix: https://yakshaG.github.io/project-pratima
+    // Do not add the slash at the last
+    IMAGE_PREFIX: isProd ? "https://yakshaG.github.io/project-pratima" : ""
+  },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/project-pratima',
-  assetPrefix: '/project-pratima/',
+  basePath: isProd ? "/project-pratima" : "",
+  assetPrefix: isProd ? "/project-pratima" : "",
   experimental: {
     images: {
       unoptimized: true,
@@ -11,6 +18,5 @@ const nextConfig = {
   },
 }
 
-const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = nextConfig
