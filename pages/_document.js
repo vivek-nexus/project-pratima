3b8fb93664d07e83dfbd9c92f154bf3e44e2634e
Vivek <audit536@gmail.com>
2022-10-01 23:28:01 +0530

    Revert "fix: :bug: Fix missing CSS on first paint"
    This reverts commit 161966f5c04bd50791173a2e3fe424dd09c6859b.

--- pages/_document.js
+++ pages/_document.js
@@ -1,48 +1,30 @@
-import Document from "next/document";
-import { ServerStyleSheet } from "styled-components";
-import { Html, Head, Main, NextScript } from "next/document";
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import loader from '../styles/loader'
 
-export default class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const sheet = new ServerStyleSheet();
-        const originalRenderPage = ctx.renderPage;
-
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
-                });
-
-            const initialProps = await Document.getInitialProps(ctx);
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            };
-        } finally {
-            sheet.seal();
-        }
-    }
-
+class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-                    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,1,0" />
-                </Head>
+                <Head />
+                <head>
+                    <style>
+                        {loader}
+                    </style>
+                </head>
                 <body>
+                    <div id={'globalLoader'}>
+                        <div className="loader">
+                            <div />
+                            <div />
+                        </div>
+                    </div>
                     <Main />
                     <NextScript />
                 </body>
             </Html>
-        );
+        )
     }
 }
+
+export default MyDocument
