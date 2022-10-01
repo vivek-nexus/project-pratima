161966f5c04bd50791173a2e3fe424dd09c6859b
Vivek <audit536@gmail.com>
2022-10-01 23:24:34 +0530

    fix: :bug: Fix missing CSS on first paint

--- pages/icons.jsx
+++ pages/icons.jsx
@@ -51,10 +51,6 @@ export default function Icons() {
                 <title>Icons</title>
                 <meta name="description" content="Payment icons for India" />
                 <link rel="icon" type="image/png" href={`${linkUrlPrefix}/favicon.png`} />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-                <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,400,1,0" />
             </Head>
 
             <Nav />
